eb16efd2e00414e1cf8cfcf85d3291b88a4df9af
achamseddine <achamseddine@unicef.org>
2018-03-12 13:28:59 +0200

    Remove static file storage

--- config/settings/production.py
+++ config/settings/production.py
@@ -78,7 +78,7 @@ INSTALLED_APPS += ['storages', ]
 
 # Static Assets
 # ------------------------
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # EMAIL
