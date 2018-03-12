626cede213d306b1731ac00ba83eef401db9c524
achamseddine <achamseddine@unicef.org>
2018-03-12 13:34:41 +0200

    Fix DJANGO_ADMIN_URL

--- config/settings/production.py
+++ config/settings/production.py
@@ -187,7 +187,7 @@ RAVEN_CONFIG = {
 }
 
 # Custom Admin URL, use {% url 'admin:index' %}
-ADMIN_URL = env('DJANGO_ADMIN_URL', '/admin/')
+ADMIN_URL = env('DJANGO_ADMIN_URL')
 
 # Your production stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
