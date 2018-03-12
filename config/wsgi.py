800002e1bc53a50b4d33abce42bfb1325e6fe0d7
achamseddine <achamseddine@unicef.org>
2018-03-12 13:05:03 +0200

    Update WSGI

--- config/wsgi.py
+++ config/wsgi.py
@@ -14,15 +14,8 @@ framework.
 
 """
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
-
-# This allows easy placement of apps within the interior
-# supplies_platform directory.
-app_path = os.path.dirname(os.path.abspath(__file__)).replace('/config', '')
-sys.path.append(os.path.join(app_path, 'supplies_platform'))
-
 if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
     from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
