c0b074f0a72e8cce017cdc47f609af2354036dfa
achamseddine <achamseddine@unicef.org>
2018-04-06 09:16:34 +0300

    Change Plan admin

--- supplies_platform/planning/admin.py
+++ supplies_platform/planning/admin.py
@@ -18,6 +18,73 @@ from .models import (
     DistributionPlanItem
 )
 
+# TabularInline
+
+
+class WavePlanFormSet(forms.BaseInlineFormSet):
+    def get_form_kwargs(self, index):
+        kwargs = super(WavePlanFormSet, self).get_form_kwargs(index)
+        kwargs['parent_object'] = self.instance
+        return kwargs
+
+
+class WavePlanInline(admin.StackedInline):
+    model = WavePlan
+    verbose_name = 'Wave'
+    verbose_name_plural = 'Waves'
+    min_num = 4
+    max_num = 4
+    suit_classes = u'suit-tab suit-tab-waves-1'
+
+    fields = (
+        'wave_number',
+        'quantity_required',
+        'date_required_by',
+    )
+
+
+class WavePlanForm(forms.ModelForm):
+
+    class Meta:
+        model = WavePlan
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        """
+        Only show supply items already in the supply plan
+        """
+        if 'parent_object' in kwargs:
+            self.parent_object = kwargs.pop('parent_object')
+
+        super(WavePlanForm, self).__init__(*args, **kwargs)
+
+        queryset = SupplyItem.objects.none()
+        if hasattr(self, 'parent_object'):
+
+            items = self.parent_object.supplyplanitem_set.all().values_list('item__id', flat=True)
+            queryset = SupplyItem.objects.filter(id__in=items)
+
+        self.fields['supply_item'].queryset = queryset
+
+
+class SupplyPlanWaveInlineAdmin(admin.StackedInline):
+    model = WavePlan
+    form = WavePlanForm
+    formset = WavePlanFormSet
+    fields = (u'supply_item', u'wave_number', u'quantity_required', u'date_required_by')
+
+    # def get_max_num(self, request, obj=None, **kwargs):
+    #     """
+    #     Only show these inlines if we have supply plans
+    #     :param request:
+    #     :param obj: SupplyPlan object
+    #     :param kwargs:
+    #     :return:
+    #     """
+    #     if obj and obj.supplyplanitem_set.count():
+    #         return self.max_num
+    #     return 0
+
 
 class SupplyPlanItemInline(admin.StackedInline):
     model = SupplyPlanItem
@@ -69,74 +136,26 @@ class SupplyPlanItemInline(admin.StackedInline):
         'quantity',
         'covered_per_item',
         'target_population',
-        'wave_number_1',
-        'wave_quantity_1',
-        'date_required_by_1',
-        'wave_number_2',
-        'wave_quantity_2',
-        'date_required_by_2',
-        'wave_number_3',
-        'wave_quantity_3',
-        'date_required_by_3',
-        'wave_number_4',
-        'wave_quantity_4',
-        'date_required_by_4',
+        # 'wave_number_1',
+        # 'wave_quantity_1',
+        # 'date_required_by_1',
+        # 'wave_number_2',
+        # 'wave_quantity_2',
+        # 'date_required_by_2',
+        # 'wave_number_3',
+        # 'wave_quantity_3',
+        # 'date_required_by_3',
+        # 'wave_number_4',
+        # 'wave_quantity_4',
+        # 'date_required_by_4',
     )
+
+    inlines = [WavePlanInline, ]
 
     readonly_fields = (
         'target_population',
         'covered_per_item',
     )
-
-
-class WavePlanFormSet(forms.BaseInlineFormSet):
-    def get_form_kwargs(self, index):
-        kwargs = super(WavePlanFormSet, self).get_form_kwargs(index)
-        kwargs['parent_object'] = self.instance
-        return kwargs
-
-
-class WavePlanForm(forms.ModelForm):
-
-    class Meta:
-        model = WavePlan
-        fields = '__all__'
-
-    def __init__(self, *args, **kwargs):
-        """
-        Only show supply items already in the supply plan
-        """
-        if 'parent_object' in kwargs:
-            self.parent_object = kwargs.pop('parent_object')
-
-        super(WavePlanForm, self).__init__(*args, **kwargs)
-
-        queryset = SupplyItem.objects.none()
-        if hasattr(self, 'parent_object'):
-
-            items = self.parent_object.supplyplanitem_set.all().values_list('item__id', flat=True)
-            queryset = SupplyItem.objects.filter(id__in=items)
-
-        self.fields['supply_item'].queryset = queryset
-
-
-class SupplyPlanWaveInlineAdmin(admin.StackedInline):
-    model = WavePlan
-    form = WavePlanForm
-    formset = WavePlanFormSet
-    fields = [u'supply_item', u'wave_number', u'quantity_required', u'date_required_by']
-
-    # def get_max_num(self, request, obj=None, **kwargs):
-    #     """
-    #     Only show these inlines if we have supply plans
-    #     :param request:
-    #     :param obj: SupplyPlan object
-    #     :param kwargs:
-    #     :return:
-    #     """
-    #     if obj and obj.supplyplanitem_set.count():
-    #         return self.max_num
-    #     return 0
 
 
 class SupplyPlanResource(resources.ModelResource):
