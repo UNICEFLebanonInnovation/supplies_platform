a7165c114964ea3ad2dcfb3e59149d6fe65e1ec7
achamseddine <achamseddine@unicef.org>
2018-05-14 09:43:29 +0300

    View site link

--- config/settings/base.py
+++ config/settings/base.py
@@ -217,6 +217,7 @@ SUIT_CONFIG = {
     'MENU_OPEN_FIRST_CHILD': True, # Default True
     'MENU_EXCLUDE': ('auth.group',),
     'MENU': (
+        {'label': 'View site', 'icon': 'icon-home', 'url': "/"},
         {'app': 'users', 'label': 'Users', 'icon':'icon-user'},
         {'app': 'supplies', 'label': 'Supplies', 'icon': 'icon-globe'},
         {'app': 'locations', 'label': 'Locations', 'icon': 'icon-globe'},
