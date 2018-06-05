51d40bd30a7eff57f83926a0f2f86bcb2ef4e216
achamseddine <achamseddine@unicef.org>
2018-06-05 11:04:17 +0300

    Correct tpm visit

--- supplies_platform/tpm/tables.py
+++ supplies_platform/tpm/tables.py
@@ -18,10 +18,9 @@ class SMVisitTable(tables.Table):
     partner = tables.Column(verbose_name='Partner', orderable=False, accessor='supply_plan_partner')
     partnership = tables.Column(verbose_name='Partnership', orderable=False, accessor='supply_plan_partnership')
     section = tables.Column(verbose_name='Section', orderable=False, accessor='supply_plan_section')
-    assign_to_tpm = tables.Column(verbose_name='Assign to TPM', orderable=False, accessor='supply_plan_tpm')
 
     assessment = tables.TemplateColumn(verbose_name='Assessment', orderable=False,
-                                                template_name='tpm/quantity_assessment.html',
+                                                template_name='tpm/quality_assessment.html',
                                                 attrs={'url': '/tpm/visits/'})
     assign_to = tables.TemplateColumn(verbose_name='Assign to me', orderable=False,
                                         template_name='tpm/assign_to.html',
@@ -52,14 +51,11 @@ class TPMVisitTable(tables.Table):
     assessment = tables.TemplateColumn(verbose_name='Quality Assessment', orderable=False,
                                                template_name='tpm/quantity_assessment.html',
                                                attrs={'url': '/tpm/visits/'})
-    assign_to = tables.TemplateColumn(verbose_name='Assign to me', orderable=False,
-                                        template_name='tpm/assign_to.html',
-                                        attrs={'url': '/tpm/visits/'})
 
     class Meta:
         model = SMVisit
         fields = (
-            'assign_to',
+            'assign_to_tpm',
             'partner',
             'partnership',
             'section',
